ca905acb70efdc5b5c37f371fa230fc9731dd991
zunovia <105263325+zunovia@users.noreply.github.com>
2022-09-21 11:15:23 +0000

    2022.9.21 colum eraced-2

--- db/migrate/20220911150116_add_column_to_post.rb
+++ db/migrate/20220911150116_add_column_to_post.rb
@@ -2,7 +2,7 @@ class AddColumnToPost < ActiveRecord::Migration[6.1]
   def change
 
     add_column :posts, :title, :string, null: false, default: '', limit: 50
-    add_column :posts, :body, :text, null: false, default: '', limit: 500
+    add_column :posts, :body, :text, null: false, limit: 500
     add_column :posts, :end_user_id, :integer
 
   end
